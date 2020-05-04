93c84b9dc54c17c37da3b434745371f35da40c3e
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2020-05-04 17:35:36 +0200

    Redesign

--- components/App.js
+++ components/App.js
@@ -1,19 +1,18 @@
 import { h } from 'hyperapp'
 
-export default (state, actions) => h('main', {}, [
-  h('div', {}, [
-    h('h1', {}, 'Hello'),
+export default (state, actions) => h('main', { class: 'main' },
+  h('div', { class: 'content' }, [
+    h('h1', { class: 'hello' }, 'hello'),
+    h('p', {}, 'My name is Jonas.'),
     h('p', {}, [
-      'My name is Jonas. I\'m a developer at ',
+      ' I\'m a developer at ',
       h('a', { href: 'https://www.jayway.com/' }, 'Jayway'),
       ' in Malmö, Sweden.'
+    ]),
+    h('div', {}, [
+      h('ul', {}, [
+        h('li', {}, h('a', { href: 'https://github.com/jonasstenberg' }, 'github')),
+        h('li', {}, h('a', { href: 'https://www.linkedin.com/in/jonasstenberg/' }, 'linkedin'))
+      ])
     ])
-  ]),
-  h('div', {}, [
-    h('h3', { class: 'heading__social' }, 'Social'),
-    h('ul', {}, [
-      h('li', {}, h('a', { href: 'https://github.com/jonasstenberg' }, 'github')),
-      h('li', {}, h('a', { href: 'https://www.linkedin.com/in/jonasstenberg/' }, 'linkedin'))
-    ])
-  ])
-])
+  ]))
